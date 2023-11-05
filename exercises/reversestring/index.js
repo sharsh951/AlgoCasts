885daffccf4273df528de90d5875bd01427e0484
Harsh <auharshitsingh@gmail.com>
2023-11-06 09:31:12 +1100

    Section 3, String Reversal

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,28 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  //   let reversedString = "";
+  //   for (let index = str.length - 1; index >= 0; index--) {
+  //     reversedString += str[index];
+  //   }
+
+  //   for (let character of str) {
+  //     reversedString = character + reversedString;
+  //   }
+
+  //   return reversedString;
+
+  //   const arr = str.split("");
+  //   return arr.reverse().join("");
+
+  //   return str.split("").reduce((reversedString, character) => {
+  //     return character + reversedString;
+  //   }, "");
+
+  //   return str.split("").reduce((rev, char) => char + rev, "");
+
+  return str.split("").reverse().join("");
+}
 
 module.exports = reverse;
